e2ce930d5cde5576f0c7cf0b91ad9e48c2743be5
Maikii <maikrichardwagner@gmail.com>
2016-01-19 12:47:31 +0100

    flyttade på min ng.js til rätt mapp (håller på med att fixa handledare backenden)

--- src/js/ng.js
+++ src/js/ng.js
@@ -7,6 +7,19 @@ module.controller("lärareCtrl",function($scope, $http){
     });
     $http.get("http://10.97.72.5:8080/aplBackend/webresources/handledare").success(function (data) {
         $scope.handledare = data;
+    });
+
+    $http.get("http://10.97.72.5:8080/aplBackend/webresources/handledare").success(function (data) {
+        $scope.handledare = data;
         console.log(data);
+        $scope.countList = [data];
+        $scope.countSelected = $scope.countList[0].id;
+        alert('Selected count ID: ' + $scope.countSelected);
+
+        $scope.onchange = function (id) {
+            alert("id:" + id.name);
+
+        };
+
     });
 });
