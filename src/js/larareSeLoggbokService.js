92b9a76fc128ea26a24d704fc93be57081a99646
dannil1212 <dannil1212@utb.vaxjo.se>
2016-02-08 15:32:08 +0100

    fixat lärarelogg

--- src/js/larareSeLoggbokService.js
+++ src/js/larareSeLoggbokService.js
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-module.service("larareloggGetService", function (http, $q){
+module.service("larareloggGetService", function ($q){
 
     this.url = SERVER_URL + "/elev";
     
