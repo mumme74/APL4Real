975e5fbf0febe330d1aa3a4d8a2484578ebcec09
Maikii <maikrichardwagner@gmail.com>
2016-02-12 12:32:18 +0100

    alla moment för elever ska fungerar nu

--- src/js/elevSeMomentController.js
+++ src/js/elevSeMomentController.js
@@ -1,19 +1,40 @@
-module.controller("elevSeMomentCtrl", function ($scope, getMoment) {
+module.controller("elevSeMomentCtrl", function ($scope, getMoment, globalService) {
     var anvandare = JSON.parse(localStorage.anvandare);
     var id_token = anvandare.id_token;
     var promiseMoment = getMoment.getMoment(id_token);
-    promiseMoment.then(function (data){
+    promiseMoment.then(function (data) {
         $scope.moment = data;
     });
-    
+
     $scope.getStatus = function (status) {
         if (status === 0)
             return "Icke avklarad";
         else if (status === 1)
             return "Väntande";
-        else if(status === 2)
+        else if (status === 2)
             return "Avklarad!";
-        
+
+    };
+
+
+    $scope.setStatus = function (id) {
+        console.log(id);
+        var data = {
+            "id": id
+        };
+        globalService.skickaData("/moment/tillHandledare", data).then(function (responses) {
+            console.log(responses);
+            location.reload();
+
+        });
+    };
+
+    $scope.Status = function (id) {
+        if (id === 0) {
+            return true;
+        } else{
+            return false;
+        }
     };
 
 });
