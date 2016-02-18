3494f89b303d98f0bd548306c421573cdacfe0df
dannil1212 <dannil1212@utb.vaxjo.se>
2016-02-18 11:58:27 +0100

    lärare redigera sina elever och HL klart funktionsmässigt

--- src/js/redigeraAnvController.js
+++ src/js/redigeraAnvController.js
@@ -3,9 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-module.controller("redigeraCtrl", function ($scope, redigeraService, globalService) {
+module.controller("redigeraCtrl", function ($scope, redigeraService, 
+                    globalService, momentService) {
     var anvandare = JSON.parse(localStorage.anvandare);
     var id_token = anvandare.id_token;
+
+    $scope.rensa = function (){
+        $scope.ddHandledare = -1;
+        document.getElementById("HLnamn").value = "";
+        document.getElementById("HLtfnr").value = "";
+        document.getElementById("HLemail").value = "";
+        document.getElementById("HLforetag").value = "";
+        document.getElementById("HLanvnamn").value = "";
+        document.getElementById("HLlosen").value = "";
+        $scope.HLp_id = -1;
+        
+        $scope.ddElev = -1;
+        document.getElementById("elevnamn").value = "";
+        document.getElementById("elevtfnr").value = "";
+        document.getElementById("elevemail").value = "";
+        $scope.elevklass = -1;
+        $scope.elevHL_id = -1;
+    };
 
     $scope.laddaAnv = function () {
         var promiseHL = redigeraService.getHL(id_token);
@@ -17,58 +36,69 @@ module.controller("redigeraCtrl", function ($scope, redigeraService, globalServi
         promiseElever.then(function (data) {
             $scope.EleverLista = data;
         });
+        
+        var promiseKlasser = momentService.getKlasser();
+        promiseKlasser.then(function (data) {
+            $scope.klasser = data;
+        });
     };
 
     $scope.laddaElev = function () {
-        var elev_id = $scope.ddElever;
-        var obj = {"elev_id": elev_id};
-        var promiseElev = redigeraService.getElev(id_token, obj);
+        var elev_id = parseInt($scope.ddElev);
+        var obj = JSON.stringify({'elev_id': elev_id});
+        var promiseElev = redigeraService.getElevInfo(id_token, obj);
         promiseElev.then(function (data) {
             $scope.elevObj = data;
+            $scope.elevklass = data.klass;
+            console.log(data.hl_id);
+            $scope.elevHL_id = data.hl_id;
+            
         });
     };
 
     $scope.laddaHL = function () {
-        var hl_id = $scope.ddHandledare;
-        var obj = {"hl_id": hl_id};
-        var promiseElev = redigeraService.getHL(id_token, obj);
+        var hl_id = parseInt($scope.ddHandledare);
+        var obj = JSON.stringify({"hl_id": hl_id});
+        var promiseElev = redigeraService.getHLInfo(id_token, obj);
         promiseElev.then(function (data) {
             $scope.HLObj = data;
+            console.log(data);
         });
     };
 
     $scope.sparaHL = function () {
         var url = "/redigera/handledare";
-        var ID = $scope.ddHandledare;
-        var namn = $scope.HLnamn;
-        var tfnr = $scope.HLtfnr;
-        var email = $scope.HLemail;
-        var program_id = $scope.HLp_id;
-        var företag = $scope.HLforetag;
-        var användarnamn = $scope.HLanvnamn;
-        var lösenord = $scope.HLlosen;
+        var ID = parseInt($scope.ddHandledare);
+        var namn = document.getElementById("HLnamn").value;
+        var tfnr = document.getElementById("HLtfnr").value;
+        var email = document.getElementById("HLemail").value;
+        var företag = document.getElementById("HLforetag").value;
+        var användarnamn = document.getElementById("HLanvnamn").value;
+        var lösenord = document.getElementById("HLlosen").value;
 
         var data = {ID: ID, "namn": namn, "tfnr": tfnr, "email": email,
-            program_id: program_id, "foretag": företag, "anvnamn": användarnamn,
+            "foretag": företag, "anvnamn": användarnamn,
             "losenord": lösenord};
 
         globalService.skickaData(url, data).then(function (responses) {
             console.log(responses);
+            $scope.rensa();
         });
     };
 
     $scope.sparaElev = function () {
         var url = "/redigera/elev";
-        var ID = $scope.ddElev;
-        var namn = $scope.elevnamn;
-        var tfnr = $scope.elevtfnr;
-        var email = $scope.elevemail;
-        var klass = $scope.elevKlass;
-        var handledar_id = $scope.elevHL_id;
+        var ID = parseInt($scope.ddElev);
+        var namn = document.getElementById("elevnamn").value;
+        var tfnr = document.getElementById("elevtfnr").value;
+        var email = document.getElementById("elevemail").value;
+        var klass = parseInt($scope.elevklass);
+        var handledar_id = parseInt($scope.elevHL_id);
         var data = {ID: ID, "namn": namn, "tfnr": tfnr, "email": email,
             klass: klass, hl_id: handledar_id};
         globalService.skickaData(url, data).then(function (responses) {
             console.log(responses);
+            $scope.rensa();
         });
     };
 });
