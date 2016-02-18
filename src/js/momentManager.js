5420ca6f007aa27111d767764e9d329e8aafd837
hanneslinner <hali93001@utb.vaxjo.se>
2016-02-18 10:17:48 +0100

    Moment se skapa tilldela halva

--- src/js/momentManager.js
+++ src/js/momentManager.js
@@ -14,22 +14,35 @@ module.controller("momentCtrler", function ($scope, momentService) {
         $scope.klasser = data;
         console.log(data);
     });
-    //elever
+
+    // hämta elever använder ng-change för att uppdatera
+    $scope.updateraElevLista = function () {
+        var anvandare = JSON.parse(localStorage.anvandare);
+        var id_token = anvandare.id_token;
+        var promiseElever = momentService.getElevFranKlass(id_token, $scope.sokKlass);
+        promiseElever.then(function (data) {
+            $scope.eleverna = data;
+            console.log(data);
+        });
+    };
+
+    //hämta moment använder ng-change för att uppdatera
+    $scope.updateraElevsMoment = function () {
+        $scope.id_token = "";
+        console.log("updateraElevsMoment");
+        var promiseMoment = momentService.allaSeMoment(id_token, $scope.sokElev);
+        promiseMoment.then(function (data) {
+            $scope.moment = data;
+        });
+    };
+    //T(m)oment
     var anvandare = JSON.parse(localStorage.anvandare);
     var id_token = anvandare.id_token;
-    var promiseElever = momentService.getElevFranKlass(id_token);
-    promiseElever.then(function (data){
-        $scope.eleverna = data;
+    var elev_id = {elev_id:$scope.sokElev}
+    var promiseToment = momentService.getToment(id_token, elev_id);
+    promiseToment.then(function (data) {
+        $scope.Toment = data;
         console.log(data);
     });
-    
-    
-    //moment
-    $scope.id_token = "";
-    var promiseMoment = momentService.allaSeMoment();
-    promiseMoment.then(function (data) {
-        $scope.moment = data;
-        console.log(data);
-    });
-    
+
 });
