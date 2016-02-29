16d0b63bfb9e8464834a6d307242fc556a65268c
Carl König <made.swede@live.com>
2016-02-29 08:35:10 +0100

    ny url + bilduppladdning url

--- src/js/globalVars.js
+++ src/js/globalVars.js
@@ -1,1 +1,2 @@
-SERVER_URL = "http://10.97.72.5:8080/aplBackend/webresources";
+SERVER_URL = "http://apl.teknikum.it:8080/aplBackend/webresources";
+IMG_SERVER_URL = "http://lundberg.t4.nu/fileupload";
