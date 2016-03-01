9e945bcb965482c3cd5e63a74a717e9c7027fa75
Carl König <made.swede@live.com>
2016-03-01 09:44:31 +0100

    oversikt controller start

--- src/js/larareOversiktCtrl.js
+++ src/js/larareOversiktCtrl.js
@@ -1,12 +1,22 @@
+module.controller("larareSeNarvaroCtrl", function ($scope, larareOversiktService, larareService) {
+    $scope.years = [];
+    $scope.start = 0;
+    $scope.currentMonth = new Date().getMonth();
+    $scope.currentYear = new Date().getFullYear();
 
-
-
-
-
-
-
-
-
+    var anvandare = JSON.parse(localStorage.anvandare);
+    var id_token = anvandare.id_token;
+    larareService.getKlasser(id_token).then(function (data) {
+        $scope.klasser = data;
+    });
+    $scope.getElever = function (klass_id) {
+        larareOversiktService.getOmdome(id_token, klass_id).then(function (data) {
+            var alla = [];
+            alla.push({elev_id: -1, namn: "Alla"});
+            $scope.elever = alla.concat(data);
+        });
+    };
+});
 
 var myPieChart = new Chart(ctx[0]).Pie(data,options);
 var data = [
