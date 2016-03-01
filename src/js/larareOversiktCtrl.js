18956f197977a16136549d99c1de6b522c6c01d6
Carl König <made.swede@live.com>
2016-03-01 10:10:11 +0100

    fixar

--- src/js/larareOversiktCtrl.js
+++ src/js/larareOversiktCtrl.js
@@ -1,12 +1,6 @@
-module.controller("larareSeNarvaroCtrl", function ($scope, larareOversiktService, larareService) {
-
-
-
-
-        $scope.labels = ["bra","sådar","dåligt", "jag vet inte"];
-        $scope.data = [10,50,20,33];
-
-
+module.controller("larareOversiktCtrl", function ($scope, larareService) {
+    $scope.labels = ["bra", "sådar", "dåligt", "jag vet inte"];
+    $scope.data = [10, 50, 20, 33];
 
     var anvandare = JSON.parse(localStorage.anvandare);
     var id_token = anvandare.id_token;
@@ -14,10 +8,12 @@ module.controller("larareSeNarvaroCtrl", function ($scope, larareOversiktService
         $scope.klasser = data;
     });
     $scope.getElever = function (klass_id) {
+        /*
         larareOversiktService.getOmdome(id_token, klass_id).then(function (data) {
             var alla = [];
             alla.push({elev_id: -1, namn: "Alla"});
             $scope.elever = alla.concat(data);
         });
+        */
     };
 });
