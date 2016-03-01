5a557a01eaa0f73e030e7f0e84eb8257ae1f0dea
Carl König <made.swede@live.com>
2016-03-01 17:18:49 +0100

    handledare reg fix

--- src/js/handledareRegController.js
+++ src/js/handledareRegController.js
@@ -4,13 +4,13 @@
  * and open the template in the editor.
  */
 
-module.controller("handledareRegCtrl", function ($scope, registrationService) {
-        var promiseProgram = registrationService.getProgram();
+module.controller("handledareRegCtrl", function ($scope, registrationService, globalService) {
+    var promiseProgram = registrationService.getProgram();
     promiseProgram.then(function (data) {
         $scope.programs = data;
         console.log(data);
     });
-    $scope.registreraHandledare = function(){
+    $scope.registreraHandledare = function () {
         console.log("handreg");
         var användarnamn = $scope.username;
         var lösenord = $scope.password;
@@ -19,14 +19,21 @@ module.controller("handledareRegCtrl", function ($scope, registrationService) {
         var namn = $scope.name;
         var foretag = $scope.frtg;
         var program = $scope.pg;
-        var promise = registrationService.registreraHandledare(användarnamn, namn, lösenord, email, tfnr, foretag, program);
-        promise.then(function (status) {
-            if (status === 200) {
-                console.log("logged in");
-                alert("Du är inloggad!");
-            } else {
-                console.log("error: Status "+status);
+        var data = {
+            användarnamn: användarnamn,
+            lösenord: lösenord,
+            email: email,
+            tfnr: tfnr,
+            namn: namn,
+            foretag: foretag,
+            program: program
+        };
+        globalService.skickaData(url, data).then(function (responses) {
+            if (responses[0].status < 200 || responses[0].status > 299)
+            {
+                alert("Ett fel inträffade, datan kommer skickas automatiskt.");
             }
+            location.reload();
         });
     };
 });
