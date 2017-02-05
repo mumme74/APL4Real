4d865f47335b7af798d11e4dcd49a2cc90affc37
Fredrik Johansson <fredrik@mumme.se>
2017-02-05 21:10:45 +0100

    Tweaked URL to not have to be changed when running dev server

--- src/js/global/globalVars.js
+++ src/js/global/globalVars.js
@@ -1,8 +1,9 @@
-LOCAL_SERVER_URL = "http://localhost:8080/aplBackend/webresources";
-REAL_SERVER_URL = "http://apl.teknikum.it:8080/aplBackend/webresources";
+//LOCAL_SERVER_URL = "http://localhost:8080/aplBackend/webresources";
+//REAL_SERVER_URL = "http://apl.teknikum.it:8080/aplBackend/webresources";
 IMG_SERVER_URL = "http://lundberg.t4.nu/fileupload";
 
-SERVER_URL = LOCAL_SERVER_URL; //REAL_SERVER_URL;
+//SERVER_URL = LOCAL_SERVER_URL; //REAL_SERVER_URL;
+SERVER_URL = location.protocol + "//" + location.hostname  + ":8080/aplBackend/webresources"; 
 
 
 var module = angular.module("apl", ["ui.router"]);
