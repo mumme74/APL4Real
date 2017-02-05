431f9bb51ba2f1172d2e73721faaacecddb5f042
Fredrik Johansson <fredrik@mumme.se>
2017-02-05 18:10:59 +0100

    Changed grubfile so it: *creates sourcemaps (easier debugging) *rebuilds on change *works as a development server (reloads on file change)

--- gruntfile.js
+++ gruntfile.js
@@ -13,15 +13,17 @@ module.exports = function (grunt) {
         uglify: {
             options: {
                 mangle: false, //behåll variabelnamn
+                banner: "/* Built: <%= grunt.template.today() %> */",
+                sourceMap: true, // makes dev tool find unmangled sources
                 compress: {
-                    drop_console: true //ta bort console.log
+                    drop_console: false//true, //ta bort console.log
                 }
             },
             aplapp: {
                 files: {
-                    'js/aplapp.min.js': [
-                        /* GLOBAL */
-                        'src/js/global/globalVars.js',
+                  //  'js/aplapp.min.js': ["src/js/**/*.js"]
+                                            /* GLOBAL */
+                    'js/aplapp.min.js'    :['src/js/global/globalVars.js',
                         'src/js/global/configAngular.js',
                         'src/js/global/globalService.js',
                         'src/js/global/loginService.js',
@@ -72,24 +74,61 @@ module.exports = function (grunt) {
                         'src/js/handledare/handledareController.js',
                         'src/js/handledare/handledareAktivitetController.js',
                         'src/js/handledare/handledareKontaktController.js',
-                        'src/js/handledare/handledareMomentController.js'
-                    ]
+                        'src/js/handledare/handledareMomentController.js']
                 }
             }
         }, //end uglify
-        copy: {
-            all: {
-                files: [
-                    {src: 'index.html', dest: 'build/index.html'},
-                    {src: 'icon.ico', dest: 'build/icon.ico'},
-                    {src: 'templates/**', dest: 'build/'},
-                    {src: 'css/**', dest: 'build/'},
-                    {src: 'js/**', dest: 'build/'},
-                    {src: 'bower_components/**', dest: 'build/'}
-
-                ]
+        watch: {
+            options: { livereload: true },
+            js: {
+                files: ["src/js/**/*.js"],
+                tasks: ["uglify", "copy:jsFiles"]
+            },
+            css: {
+                files: ["css/**/*.css"],
+                tasks: ["copy:cssFiles"]
+            },
+            templates: {
+                files: ["templates/**"],
+                tasks: ["copy:templates"]
+            },
+            htmFiles: {
+                files: ["*.htm", "*.html"],
+                tasks: ["copy:htmFiles"]
             }
+        },
+        express: {
+          // starting a development webserver (autoreloads page when a file is changed)
+          all: {
+              options:{
+                  livereload:true,
+                  port: 9000,
+                  bases: './',
+                  hostname: '*' // allow 127.0.0.1:9000
+              }
+          }  
+        },
+        copy: {
+            cssFiles: { files: [{src: 'css/**', dest: 'build/'}] },
+            htmFiles: { files: [{src: 'index.html', dest: 'build/index.html'}]},
+            iconFile: { files: [{src: 'icon.ico', dest: 'build/icon.ico'}]},
+            templates:{ files: [{src: 'templates/**', dest: 'build/'}]},
+            jsFiles:  { files: [{src: 'js/**', dest: 'build/'}]},
+            bower:    { files: [{src: 'bower_components/**', dest: 'build/'}]}
+            
+//            all: {
+//                files: [
+//                    {src: 'index.html', dest: 'build/index.html'},
+//                    {src: 'icon.ico', dest: 'build/icon.ico'},
+//                    {src: 'templates/**', dest: 'build/'},
+//                    {src: 'css/**', dest: 'build/'},
+//                    {src: 'js/**', dest: 'build/'},
+//                    {src: 'bower_components/**', dest: 'build/'}
+//
+//                ]
+//            }
         }, //end copy
+        
         "ftp-deploy": {
             build: {
                 auth: {
@@ -108,11 +147,18 @@ module.exports = function (grunt) {
     //copy tasks
     grunt.loadNpmTasks('grunt-contrib-copy');
     //ftp task
-     grunt.loadNpmTasks('grunt-ftp-deploy');
+    grunt.loadNpmTasks('grunt-ftp-deploy');
+    // watch for changes in filesystem
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    // a automatic relaoding webserver
+    grunt.loadNpmTasks('grunt-express');
     
     // Default task(s).
     grunt.registerTask('build', ['uglify']);
     //Send to server
     grunt.registerTask('deploy', ['copy','ftp-deploy']);
+    
+    // run as a dev server (autoreloads when files are saved)
+    grunt.registerTask('server', ['express', 'watch']);
 
 };
